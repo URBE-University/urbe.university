e25b15308c098dfb195eb1eca0ea2c70e75f29a4
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-01-19 14:47:01 -0500

    ..

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\Post\Index as Posts;
 use App\Http\Livewire\Admin\Event\Index as Events;
-use App\Http\Livewire\Admin\User\FinishSetup;
 use App\Http\Livewire\Admin\User\Index as Users;
 
 /*
@@ -24,25 +23,22 @@ Route::get('/', function () {
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified'
+    'verified',
+    'is_active'
 ])->prefix('admin')->group(function () {
-    Route::get('/finish-setup', FinishSetup::class)->name('admin.finish-setup');
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 
-    Route::middleware('is_active')->group(function () {
-        Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+    // Article routes
+    Route::middleware('can:post:view')->get('/posts', Posts::class)->name('admin.posts');
+    Route::middleware('can:post:create')->get('/posts/new', App\Http\Livewire\Admin\Post\Create::class)->name('admin.post.create');
+    Route::middleware('can:post:update')->get('/posts/{post}/edit', App\Http\Livewire\Admin\Post\Edit::class)->name('admin.post.edit');
 
-        // Article routes
-        Route::middleware('can:post:view')->get('/posts', Posts::class)->name('admin.posts');
-        Route::middleware('can:post:create')->get('/posts/new', App\Http\Livewire\Admin\Post\Create::class)->name('admin.post.create');
-        Route::middleware('can:post:update')->get('/posts/{post}/edit', App\Http\Livewire\Admin\Post\Edit::class)->name('admin.post.edit');
+    // Event routes
+    Route::middleware('can:event:view')->get('/events', Events::class)->name('admin.events');
+    Route::middleware('can:event:create')->get('/events/new', App\Http\Livewire\Admin\Event\Create::class)->name('admin.event.create');
+    Route::middleware('can:event:update')->get('/events/{event}/edit', App\Http\Livewire\Admin\Event\Edit::class)->name('admin.event.edit');
 
-        // Event routes
-        Route::middleware('can:event:view')->get('/events', Events::class)->name('admin.events');
-        Route::middleware('can:event:create')->get('/events/new', App\Http\Livewire\Admin\Event\Create::class)->name('admin.event.create');
-        Route::middleware('can:event:update')->get('/events/{event}/edit', App\Http\Livewire\Admin\Event\Edit::class)->name('admin.event.edit');
-
-        // User routes
-        Route::middleware('can:user:view')->get('/users', Users::class)->name('admin.users');
-        Route::middleware('can:user:update')->get('/user/{user}/edit', App\Http\Livewire\Admin\User\Edit::class)->name('admin.user.edit');
-    });
+    // User routes
+    Route::middleware('can:user:view')->get('/users', Users::class)->name('admin.users');
+    Route::middleware('can:user:update')->get('/user/{user}/edit', App\Http\Livewire\Admin\User\Edit::class)->name('admin.user.edit');
 });
