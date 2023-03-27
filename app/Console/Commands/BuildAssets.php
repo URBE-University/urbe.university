de08ebcb2621654dbb1a3ce92fd176b4ed75e28a
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-03-27 13:44:00 -0400

    ..

--- app/Console/Commands/BuildAssets.php
+++ app/Console/Commands/BuildAssets.php
@@ -36,8 +36,8 @@ class BuildAssets extends Command
             Storage::put('custom_stubs/' . $page->url . '.html', $page->content);
         }
 
-        dump(
-            shell_exec("cd ./../ && php -r exec('npm run build');")
-        );
+        $result = shell_exec("cd ./../ && npm run build");
+
+        dump($result);
     }
 }
