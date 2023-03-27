17523ab9cd62f2a8807edaf490d8f5c2b1dd7c62
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-03-27 14:32:57 -0400

    ..

--- app/Console/Commands/BuildAssets.php
+++ app/Console/Commands/BuildAssets.php
@@ -42,7 +42,7 @@ class BuildAssets extends Command
 
         // dump($result->errorOutput());
 
-        $result = shell_exec("cd ./../ && sudo -u www-data npm run build");
+        $result = shell_exec("cd ./../ && npm run build");
 
         dd($result);
     }
