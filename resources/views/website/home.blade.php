e7fdb18b8d7f15f2aeadddfcde9b87ac35e1a3e0
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-04-03 08:58:02 -0400

    ..

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -6,13 +6,13 @@
         {{-- Header Content --}}
         <div class="z-20 relative h-full w-full p-5 text-white bg-urbe bg-opacity-80 flex items-center justify-center">
             <div class="max-w-5xl">
-                <h1 class="text-5xl lg:text-7xl font-black">Welcome to URBE University</h1>
+                <h1 class="text-4xl md:text-5xl lg:text-7xl font-black">Welcome to URBE University</h1>
                 <p class="mt-6 text-lg md:text-xl">Explore our undergraduate and graduate programs in business, communications, education and more.</p>
                 <div class="mt-6 flex items-center space-x-4">
-                    <a href="" class="inline-block text-slate-700 bg-white focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-base px-5 py-2 md:px-6 md:py-3 mr-1 md:mr-0 focus:outline-none"
+                    <a href="" class="inline-block text-slate-700 bg-white focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-md sm:text-base px-5 py-2 md:px-6 md:py-3 mr-1 md:mr-0 focus:outline-none"
                     >{{ __("Learn more about our programs") }}</a>
                     <a href="https://admissions.urbe.university" target="_blank" dofollow
-                        class="inline-block text-white bg-sky-500 hover:bg-sky-400 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-base px-5 py-2 md:px-6 md:py-3 mr-1 md:mr-0 focus:outline-none"
+                        class="inline-block text-white bg-sky-500 hover:bg-sky-400 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-md sm:text-base px-5 py-2 md:px-6 md:py-3 mr-1 md:mr-0 focus:outline-none"
                     >{{ __("Apply now") }}</a>
                 </div>
             </div>
