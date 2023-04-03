aba30f0d08f9eb1c3e1fcd46e26a46407eecf2ad
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-04-03 10:57:20 -0400

    centered buttons on cta

--- storage/framework/views/c655ca3338946b932280ee6876916e4e.php
+++ storage/framework/views/c655ca3338946b932280ee6876916e4e.php
@@ -1,7 +1,7 @@
 <div class="py-12 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
     <h2 class="text-4xl text-urbe font-serif font-bold capitalize text-center"><?php echo e(__("Are you ready to start today?")); ?></h2>
     <p class="mt-4 text-center">Supercharge your future with a degree from URBE University. We work closely with you to ensure you are well-informed throughout your application process. Start your application today, or explore our programs to learn more about what we have to offer.</p>
-    <div class="mt-6 flex items-center sm:space-x-4">
+    <div class="mt-6 flex items-center justify-center sm:space-x-4">
         <a href="" class="inline-block text-slate-600 bg-slate-100 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-sm sm:text-base px-5 py-2 md:px-6 md:py-3 mr-1 md:mr-0 focus:outline-none"
         ><?php echo e(__("Learn more about our programs")); ?></a>
         <a href="https://admissions.urbe.university" target="_blank" dofollow
