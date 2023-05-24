2ffc364c5021387145615972c0109f1a9e1c2ebf
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-05-24 12:24:50 -0400

    ..

--- templates/ready/about/contact-us.blade.php
+++ templates/ready/about/contact-us.blade.php
@@ -2,26 +2,26 @@
     <div class="mx-auto h-full max-w-5xl px-4 sm:px-6 lg:px-8">
       <div class="py-12 text-center text-white sm:py-24">
         <h1 class="text-5xl font-extrabold">About URBE University</h1>
-        <p class="text-lg mt-6 text-lg md:text-xl">Get to know who we are, what we do, and why we do it.</p>
+        <p class="mt-6 text-lg md:text-xl">Get to know who we are, what we do, and why we do it.</p>
       </div>
     </div>
   </div>
   <section class="w-full pb-24 pt-16">
     <div class="mx-auto max-w-5xl px-4 text-center sm:px-6 lg:px-8">
       <div class="grid grid-cols-6 items-start gap-8">
-        <main class="col-span-6 min-h-screen text-left md:col-span-4">
+        <main class="col-span-6 text-left md:col-span-4">
           <h2 class="text-urbe text-3xl font-bold">{!!__('Contact Us')!!}</h2>
           <div class="mt-6">
             <p>A complete listing of our departments contact information is provided below. If you need help with general inquiries, please call <a href="tel:+18447448723" class="text-sky-600 underline">+1 844-744-8723</a> or email <a href="mailto:info@urbeuniversity.edu" class="text-sky-600 underline">info@urbeuniversity.edu</a></p>
 
             <div class="mt-6 grid grid-cols-2 gap-4 text-sm">
-              <div class="col-span-2 md:col-span-1 border border-slate-100 p-4 rounded-md bg-white">
-                <h4 class="text-lg font-semibold text-urbe">Admissions</h4>
+              <div class="col-span-2 rounded-md border border-slate-100 bg-white p-4 md:col-span-1">
+                <h4 class="text-urbe text-lg font-semibold">Admissions</h4>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                   </svg>
-                  <span>+1 844-744-8723</span>
+                  <span>+1 844-744-8723 <span class="text-xs">(Toll free)</span></span>
                 </div>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
@@ -34,19 +34,19 @@
               </div>
 
               <!-- Registrar -->
-              <div class="col-span-2 md:col-span-1 border border-slate-100 p-4 rounded-md bg-white">
-                <h4 class="text-lg font-semibold text-urbe">Registrar</h4>
+              <div class="col-span-2 rounded-md border border-slate-100 bg-white p-4 md:col-span-1">
+                <h4 class="text-urbe text-lg font-semibold">Registrar</h4>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                   </svg>
-                  <span>+1 844-744-8723</span>
+                  <span>+1 844-744-8723 ext. 202 <span class="text-xs">(Toll free)</span></span>
                 </div>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                   </svg>
-                  <span>+1 844-744-8723</span>
+                  <span>+1 786-744-8202</span>
                 </div>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
@@ -59,19 +59,19 @@
               </div>
 
               <!-- Bursar -->
-              <div class="col-span-2 md:col-span-1 border border-slate-100 p-4 rounded-md bg-white">
-                <h4 class="text-lg font-semibold text-urbe">Bursar</h4>
+              <div class="col-span-2 rounded-md border border-slate-100 bg-white p-4 md:col-span-1">
+                <h4 class="text-urbe text-lg font-semibold">Bursar</h4>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                   </svg>
-                  <span>+1 844-744-8723</span>
+                  <span>+1 844-744-8723 ext. 209 <span class="text-xs">(Toll free)</span></span>
                 </div>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                   </svg>
-                  <span>+1 844-744-8723</span>
+                  <span>+1 786-744-8209</span>
                 </div>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
@@ -84,19 +84,19 @@
               </div>
 
               <!-- Library -->
-              <div class="col-span-2 md:col-span-1 border border-slate-100 p-4 rounded-md bg-white">
-                <h4 class="text-lg font-semibold text-urbe">Library</h4>
+              <div class="col-span-2 rounded-md border border-slate-100 bg-white p-4 md:col-span-1">
+                <h4 class="text-urbe text-lg font-semibold">Library</h4>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                   </svg>
-                  <span>+1 844-744-8723</span>
+                  <span>+1 844-744-8723 ext. 211 <span class="text-xs">(Toll free)</span></span>
                 </div>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                   </svg>
-                  <span>+1 844-744-8723</span>
+                  <span>+1 786-744-8211</span>
                 </div>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
@@ -109,19 +109,13 @@
               </div>
 
               <!-- Student services -->
-              <div class="col-span-2 md:col-span-1 border border-slate-100 p-4 rounded-md bg-white">
-                <h4 class="text-lg font-semibold text-urbe">Student Services</h4>
+              <div class="col-span-2 rounded-md border border-slate-100 bg-white p-4 md:col-span-1">
+                <h4 class="text-urbe text-lg font-semibold">Student Services</h4>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                   </svg>
-                  <span>+1 844-744-8723</span>
-                </div>
-                <div class="mt-2 flex items-center space-x-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                    <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
-                  </svg>
-                  <span>+1 844-744-8723</span>
+                  <span>+1 844-744-8723 ext. 202 <span class="text-xs">(Toll free)</span></span>
                 </div>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
@@ -129,24 +123,18 @@
                     <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
                   </svg>
 
-                  <span>registrar@urbeuniversity.edu</span>
+                  <span>stuserv@urbeuniversity.edu</span>
                 </div>
               </div>
 
               <!-- Technical Support -->
-              <div class="col-span-2 md:col-span-1 border border-slate-100 p-4 rounded-md bg-white">
-                <h4 class="text-lg font-semibold text-urbe">Technical Support</h4>
+              <div class="col-span-2 rounded-md border border-slate-100 bg-white p-4 md:col-span-1">
+                <h4 class="text-urbe text-lg font-semibold">Technical Support</h4>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                   </svg>
-                  <span>+1 844-744-8723</span>
-                </div>
-                <div class="mt-2 flex items-center space-x-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                    <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
-                  </svg>
-                  <span>+1 844-744-8723</span>
+                  <span>+1 844-744-8723 ext. 108 <span class="text-xs">(Toll free)</span></span>
                 </div>
                 <div class="mt-2 flex items-center space-x-3">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
@@ -154,69 +142,11 @@
                     <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
                   </svg>
 
-                  <span>registrar@urbeuniversity.edu</span>
+                  <span>support@urbeuniversity.edu</span>
                 </div>
               </div>
             </div>
           </div>
-
-          <!-- <div class="relative mt-6 overflow-x-auto shadow-md sm:rounded-md">
-            <table class="w-full text-left text-sm text-gray-700">
-              <thead class="bg-urbe text-xs uppercase text-white">
-                <tr>
-                  <th scope="col" class="px-6 py-3">Department</th>
-                  <th scope="col" class="px-6 py-3">Phone number</th>
-                  <th scope="col" class="px-6 py-3 text-right">E-mail address</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">Admissions</th>
-                  <td class="px-6 py-4">+1 844-744-8723</td>
-                  <td class="px-6 py-4 text-right">admissions@urbeuniversity.edu</td>
-                </tr>
-                <tr>
-                  <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">Registrar</th>
-                  <td class="px-6 py-4">
-                    <ul>
-                      <li>+1 844-744-8723 #202</li>
-                      <li>+1 786-744-8202</li>
-                    </ul>
-                  </td>
-                  <td class="px-6 py-4 text-right">registrar@urbeuniversity.edu</td>
-                </tr>
-                <tr>
-                  <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">Bursar</th>
-                  <td class="px-6 py-4">
-                    <ul>
-                      <li>+1 844-744-8723 #209</li>
-                      <li>+1 786-744-8209</li>
-                    </ul>
-                  </td>
-                  <td class="px-6 py-4 text-right">bursar@urbeuniversity.edu</td>
-                </tr>
-                <tr>
-                  <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">Library</th>
-                  <td class="px-6 py-4">
-                    <ul>
-                      <li>+1 844-744-8723 #104</li>
-                      <li>+1 786-744-8211</li>
-                    </ul>
-                  </td>
-                  <td class="px-6 py-4 text-right">library@urbeuniversity.edu</td>
-                </tr>
-                <tr>
-                  <th scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">Technical Support</th>
-                  <td class="px-6 py-4">
-                    <ul>
-                      <li>+1 844-744-8723 #108</li>
-                    </ul>
-                  </td>
-                  <td class="px-6 py-4 text-right">support@urbeuniversity.edu</td>
-                </tr>
-              </tbody>
-            </table>
-          </div> -->
         </main>
 
         <!-- Sidenav -->
