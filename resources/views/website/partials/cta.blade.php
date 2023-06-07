e9bcdc77a907dd384c72319afc985965fd64a6dc
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-06-07 13:14:26 -0400

    ..

--- resources/views/website/partials/cta.blade.php
+++ resources/views/website/partials/cta.blade.php
@@ -2,7 +2,7 @@
     <h2 class="text-4xl text-urbe font-serif font-bold capitalize text-center">{{ __("Are you ready to start today?") }}</h2>
     <p class="mt-4 text-center">Supercharge your future with a degree from URBE University. We work closely with you to ensure you are well-informed throughout your application process. Start your application today, or explore our programs to learn more about what we have to offer.</p>
     <div class="mt-6 flex items-center justify-center sm:space-x-4">
-        <a href="" class="inline-block text-slate-600 bg-slate-100 focus:ring-4 focus:ring-sky-300 font-medium rounded-md text-sm sm:text-base px-5 py-2 md:px-6 md:py-3 mr-1 md:mr-0 focus:outline-none"
+        <a href="/academics" class="inline-block text-slate-600 bg-slate-100 focus:ring-4 focus:ring-sky-300 font-medium rounded-md text-sm sm:text-base px-5 py-2 md:px-6 md:py-3 mr-1 md:mr-0 focus:outline-none"
         >{{ __("Learn more about our programs") }}</a>
         <a href="https://admissions.urbe.university" target="_blank" dofollow
             class="inline-block text-white bg-sky-500 hover:bg-sky-400 focus:ring-4 focus:ring-sky-300 font-medium rounded-md text-sm sm:text-base px-5 py-2 md:px-6 md:py-3 mr-1 md:mr-0 focus:outline-none"
