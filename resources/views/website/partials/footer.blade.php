cf044201d5072df5aac23d28c7cdeb5dae2f170a
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-06-09 12:43:00 -0400

    ..

--- resources/views/website/partials/footer.blade.php
+++ resources/views/website/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full bg-urbe text-white text-base font-light">
+<footer class="w-full bg-urbe text-white text-base font-light">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div class="grid grid-cols-4 gap-8">
             <div class="col-span-4 md:col-span-2 lg:col-span-1">
@@ -71,4 +71,4 @@
             </div>
         </div>
     </div>
-</div>
+</footer>
