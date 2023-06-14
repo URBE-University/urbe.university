4bcab5ee0deb1418f8d26b7b2042e5223429fb40
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-06-14 10:49:24 -0400

    ..

---
+++ templates/ready/admissions/credit-transfer-guide.blade.php
@@ -0,0 +1,112 @@
+<div class="w-full" style="background-image: url(https://dev.urbe.university/static_assets/page-header-bg.webp)">
+    <div class="mx-auto h-full max-w-5xl px-4 sm:px-6 lg:px-8">
+      <div class="py-12 text-white sm:py-24">
+        <h1 class="text-5xl font-extrabold">{{ __("Credit Transfer Guide") }}</h1>
+        <p class="text-lgmt-6 mt-6 text-lg md:text-xl">{{__("")}}</p>
+      </div>
+    </div>
+  </div>
+  <section class="w-full pb-24 pt-16">
+    <div class="mx-auto max-w-5xl px-4 text-center sm:px-6 lg:px-8">
+      <div class="grid grid-cols-6 items-start gap-8">
+        <main class="col-span-6 min-h-screen text-left md:col-span-4">
+            <p class="text-lg font-medium italic">
+                {{__("Welcome to URBE University, where your undergraduate/graduate journey begins! We are delighted that you have chosen URBE as your educational destination. To ensure a smooth admissions process, please carefully review the following guidelines.")}}
+            </p>
+
+            <div class="mt-6">
+                <h2 class="text-urbe text-3xl font-bold">{{__("What do you need to begin your application?")}}</h2>
+                <ul class="mt-3 ml-4 list-decimal">
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("Submit your Transcripts:") }}</span>
+                        <span>{{ __("As an applicant, you are required to provide transcripts from your completed bachelor's degree/high school diploma. Please note that a degree from a specific field is not required.") }}</span>
+                    </li>
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("Complete your Admission Application:") }}</span>
+                        <span>{{ __("Fill out our online admission application, available on our public website at https://urbeuniversity.edu. We strongly recommend completing this form with accurate and up-to-date information.") }}</span>
+                    </li>
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("Pay Application Fee:") }}</span>
+                        <span>{{ __("A $100 non-refundable application fee must be submitted along with your application. This fee helps cover the administrative costs associated with processing your application. You can submit your application without the fee, but this may delay your application process until the fee is paid to our Bursar office.") }}</span>
+                    </li>
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("Government-issued Identification:") }}</span>
+                        <span>{{ __("Provide a copy of a valid government-issued ID. You can do this while you complete your online application process. Alternatively, you can send a COLOR copy of your document in PDF format to our Admissions department.") }}</span>
+                    </li>
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("Internet Access:") }}</span>
+                        <span>{{ __("If you plan to pursue your studies online, ensure you have a stable internet connection. Please review the minimum requirements specified in our Catalog under the Distance Learning section.") }}</span>
+                    </li>
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("Evaluation of International Academic Credentials(if applicable):") }}</span>
+                        <span>{{ __("If you earned academic credentials from institutions outside the United States, they must be evaluated by an official evaluation agency as per our policy included in our catalog.") }}</span>
+                    </li>
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("English and Spanish Language Proficiency:") }}</span>
+                        <ul class="mt-3 ml-4 list-disc">
+                            <li>
+                                <span class="font-semibold text-urbe">{{ __("English track:") }}</span>
+                                <span>{{ __("If your primary language is not English and you intend to enroll in the English Track, you must provide proof of English proficiency as per our policy outlined in the catalog.") }}</span>
+                            </li>
+                            <li>
+                                <span class="font-semibold text-urbe">{{ __("Spanish track:") }}</span>
+                                <span>{{ __("Similarly, if your primary language is not Spanish and you wish to enroll in the Spanish Track, you must provide proof of Spanish proficiency as per our policy outlined in the catalog.") }}</span>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="mt-6">
+                <h2 class="text-urbe text-3xl font-bold">{{__("Admissions Process")}}</h2>
+                <p class="mt-3">
+                    {{__("At URBE University, we are committed to assisting you throughout the admissions process. Here's an overview of how it works:")}}
+                </p>
+
+                <ul class="ml-4 mt-3 list-decimal">
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("Online Application:") }}</span>
+                        <span>{{ __("Start by completing the online application available on our public website at https://urbeuniversity.edu. Once submitted, an admission advisor will initiate the process by conducting a preliminary interview and providing information on admission requirements, available program delivery modalities (on site or online), and instructions for document submission and evaluation.") }}</span>
+                    </li>
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("Document Submission and Evaluation:") }}</span>
+                        <span>{{ __("Follow the instructions provided by your admission advisor to submit all required documents. These documents will be reviewed to determine your eligibility for the proposed program of enrollment. It's essential to ensure all admission requirements have been satisfied.") }}</span>
+                    </li>
+                    <li>
+                        <span class="font-semibold text-urbe">{{ __("Program Orientation:") }}</span>
+                        <span>{{ __("Once you have met all admission requirements, your admission advisor will schedule a program orientation session before the start of the semester. This orientation will help familiarize you with your chosen program and answer any questions you may have.") }}</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="mt-6">
+                <h2 class="text-urbe text-3xl font-bold">{{__("Catalog and Admission Advisors")}}</h2>
+                <p class="mt-3">
+                    {{__("URBE University's policies, procedures, and requirements are all detailed in our catalog. We encourage you to thoroughly read through the catalog and inform your admission advisor of any questions or concerns you may have.")}}
+                </p>
+                <p class="mt-3">
+                    {{__("Our admission advisors are available to assist you during normal business hours. You can reach them in person, via phone, email, WhatsApp, or video conference. To request an appointment or address any inquiries, please email admissions@urbeuniversity.edu. We are here to support you every step of the way.")}}
+                </p>
+            </div>
+        </main>
+
+        <!-- Sidenav -->
+        <aside class="col-span-6 hidden justify-end text-base md:col-span-2 md:flex">
+          <ul class="text-left">
+            <li class="mt-2">
+              <p class="border-l-urbe text-urbe border-l-4 pl-2 text-xl font-semibold">{{ __("In this section") }}</p>
+            </li>
+            <li class="mt-4">
+              <a href="/admissions" class="transition-all hover:text-sky-500 hover:underline">{{ __("Admissions Guide") }}</a>
+            </li>
+            <li class="mt-2">
+              <a href="/admissions/credit-transfer-guide" class="transition-all hover:text-sky-500 hover:underline">{{ __("Credit Transfer Guide") }}</a>
+            </li>
+            <li class="mt-2">
+              <a href="/admissions/costs-and-aid" class="transition-all hover:text-sky-500 hover:underline">{{ __("Costs & Aid") }}</a>
+            </li>
+          </ul>
+        </aside>
+      </div>
+    </div>
+  </section>
