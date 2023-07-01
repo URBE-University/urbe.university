582d7a847cf70332f6d83f293c814ab00f54c29b
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-07-01 00:45:28 -0400

    ..

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     {{-- Header --}}
-    <header class="relative flex items-center justify-center h-96 md:h-[calc(100vh-74px)] mb-12 overflow-hidden">
+    <header class="relative flex items-center justify-center md:h-[calc(100vh-74px)] mb-12 overflow-hidden">
         {{-- Header Content --}}
-        <div class="z-20 relative h-full w-full p-5 text-white bg-urbe bg-opacity-80 flex items-center justify-center">
+        <div class="z-20 relative h-full w-full p-5 py-12 text-white bg-urbe bg-opacity-80 flex items-center justify-center">
             <div class="max-w-5xl px-4 sm:px-6 lg:px-8">
                 <h1 class="text-5xl lg:text-6xl font-black">Welcome to URBE University</h1>
                 <p class="mt-6 text-lg md:text-xl">Explore our undergraduate and graduate programs in business, communications, education and more.</p>
